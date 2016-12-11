1dd346dc83664c1cbfc648a60262efedd2040c6e
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2016-12-11 19:43:32 +0700

    add python 2

--- installpython.ps1
+++ installpython.ps1
@@ -34,9 +34,31 @@ switch ($py)
 		Write-Host -Object 'Python 2';
 		if ($Arch -eq 'x86') {
 			Write-Host -Object 'Running 32-bit PowerShell';
+
+			$url1 = "https://github.com/wannaphongcom/autoinstallpythonforwindows/raw/master/data/py27-32.txt"
+			$output1 = "$PSScriptRoot\link-py2.txt"
+			Invoke-WebRequest -Uri $url1 -OutFile $output1
+			$url = Get-Content -Path $PSScriptRoot\link-py2.txt
+			$output = "$PSScriptRoot\python2-32bit.exe"
+			$start_time = Get-Date
+			Invoke-WebRequest -Uri $url -OutFile $output
+			Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+			[System.Diagnostics.Process]::Start("$PSScriptRoot\python2-32bit.exe")
+
 		}
 		elseif ($Arch -eq 'amd64') {
 			Write-Host -Object 'Running 64-bit PowerShell';
+
+			$url1 = "https://github.com/wannaphongcom/autoinstallpythonforwindows/raw/master/data/py27-64.txt"
+			$output1 = "$PSScriptRoot\link-py2.txt"
+			Invoke-WebRequest -Uri $url1 -OutFile $output1
+			$url = Get-Content -Path $PSScriptRoot\link-py2.txt
+			$output = "$PSScriptRoot\python2-64bit.exe"
+			$start_time = Get-Date
+			Invoke-WebRequest -Uri $url -OutFile $output
+			Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+			[System.Diagnostics.Process]::Start("$PSScriptRoot\python2-64bit.exe")
+
 		}
 	}
 }
